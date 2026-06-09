0389afe205e9a5aa5f5c888484c7c199a2506b05
Dummy Mail <dummymaill123@gmail.com>
2026-06-09 21:21:45 +0000

    Add 2nd meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
         
-        {/* Meta Pixel Code */}
+        {/* Brand Hercules Meta Pixel Code */}
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -34,6 +34,23 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
+
+         {/* Beauty Products Meta Pixel Code */}
+          <Script>
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1024196946969603');
+          fbq('track', 'PageView');
+        `}
+          </Script>
+         
       </head>
       <body className="font-body antialiased bg-[#F2F2F0] text-[#1A1A1A]">
         <noscript>
